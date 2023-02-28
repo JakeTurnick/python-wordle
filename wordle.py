47078429e1fe31a9a80af6361454f1597c4b3c79
JakeTurnick <jaketurnick@gmail.com>
2023-02-28 11:46:41 -0500

    more fixes, duplicate letters still bugged

--- wordle.py
+++ wordle.py
@@ -2,15 +2,19 @@ import random
 from colorama import Fore, Back, Style, init
 init(autoreset=True)
 
+TEST_WORD = "apple"
 words = open('/usr/share/dict/words', 'r')
 newWords = []
 
 for word in words:
     if len(word) == 6:
         newWords.append(word)
+words.close()
 
-word = random.choice(newWords).lower()
-TEST_WORD = "clear"
+word = random.choice(newWords).lower().strip()
+# word = TEST_WORD
+# print("Hint - " + word)
+
 
 curr_wordle = "-----"
 attemps = -1  # amount of attempts so far
@@ -19,43 +23,56 @@ attemps = -1  # amount of attempts so far
 # also if using repeated characters ...
 # ... All instances of that character are the same color
 # ... ex: word = clear -> rrrrr (all yellow, last should be green)
+# print("Hint -", word)
 
 guess = input(Fore.RESET + "Guess a 5 letter word: ")
 while len(guess) != 5:
     guess = input(Fore.RESET + "Word must be 5 characters long: ")
 
+
 while attemps < 10:
     attemps += 1
     attemp = ""
     guide = ""
+    Fore.RESET
 
-    if attemps > 5:
+    if attemps > 6:
+        Fore.RESET
         print(f"You lost! The word was {Fore.RED + word}")
         break
 
     for char in guess:
         if word.__contains__(char):
+
             # print(f"{TEST_WORD} contains the letter {char} at {guess.index(char)}")
+            # print(char + " is at word " + str(word.index(char)))
+            # print(char + " is at guess " + str(guess.index(char)))
             if word.index(char) == guess.index(char):
                 # print(f"{char} is in the right place")
                 # print green
                 attemp += (Fore.GREEN + char)
                 guide += (Fore.GREEN + char)
+                Style.RESET_ALL
             # print yellow
             else:
                 attemp += (Fore.YELLOW + char)
                 guide += (Fore.YELLOW + char)
+                Style.RESET_ALL
         # add character no style
         else:
             attemp += (Fore.RESET + char)
             guide += (Fore.RESET + "-")
     Fore.RESET
-    # print("attemp: ", attemp, " word: ", TEST_WORD)
+
     if guess.lower() == word.lower():
         print("you win!\nThe word was - " + Fore.CYAN + guess.lower())
         break
 
-    print(Style.RESET_ALL + f"Attempts remaining: {attemps}/6")
-    guess = input(Style.RESET_ALL + guide + "\n")
+    print(Fore.RESET + f"Attempts remaining: {attemps}/6")
+    guess = input(Fore.RESET + guide + "\n")
     while len(guess) != 5:
         guess = input(Fore.RESET + "Word must be 5 characters long: ")
+
+    # guess = input(Fore.RESET + "Guess a 5 letter word: ")
+    # while len(guess) != 5:
+    #     guess = input(Fore.RESET + "Word must be 5 characters long: ")
