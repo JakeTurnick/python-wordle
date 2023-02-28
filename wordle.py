12d0dbe2bab98a5f5af328eef8ffba0bf96a57c2
JakeTurnick <jaketurnick@gmail.com>
2023-02-28 15:23:57 -0500

    removed comments for clarity

--- wordle.py
+++ wordle.py
@@ -19,12 +19,6 @@ print("Hint - " + word)
 curr_wordle = "-----"
 attemps = -1  # amount of attempts so far
 
-# Colors are leaking into the input, not sure why
-# also if using repeated characters ...
-# ... All instances of that character are the same color
-# ... ex: word = clear -> rrrrr (all yellow, last should be green)
-# print("Hint -", word)
-
 guess = input(Fore.RESET + "Guess a 5 letter word: ")
 while len(guess) != 5:
     guess = input(Fore.RESET + "Word must be 5 characters long: ")
@@ -40,28 +34,6 @@ while attemps < 10:
         Fore.RESET
         print(f"You lost! The word was {Fore.RED + word}")
         break
-
-# the issue is the index is checked by the first instance of the letter
-    # for char in guess:
-    #     if word.__contains__(char):
-    #         # print(f"{TEST_WORD} contains the letter {char} at {guess.index(char)}")
-    #         if word.index(char) == guess.index(char):
-    #             # print(f"{char} is in the right place")
-    #             # print green
-    #             attemp += (Fore.GREEN + char)
-    #             guide += (Fore.GREEN + char)
-    #             Style.RESET_ALL
-    #         # print yellow
-    #         else:
-    #             attemp += (Fore.YELLOW + char)
-    #             guide += (Fore.YELLOW + char)
-    #             Style.RESET_ALL
-    #     # add character no style
-    #     else:
-    #         attemp += (Fore.RESET + char)
-    #         guide += (Fore.RESET + "-")
-    #         guide_check[guess.index(char)] = ""
-    # Fore.RESET
 
     for i in range(0, 5):
         if word.__contains__(guess[i]):
@@ -88,7 +60,3 @@ while attemps < 10:
     guess = input(Fore.RESET + guide + "\n")
     while len(guess) != 5:
         guess = input(Fore.RESET + "Word must be 5 characters long: ")
-
-    # guess = input(Fore.RESET + "Guess a 5 letter word: ")
-    # while len(guess) != 5:
-    #     guess = input(Fore.RESET + "Word must be 5 characters long: ")
